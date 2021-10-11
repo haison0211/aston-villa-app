3e0e0a363a932807e5be1a47fd371dafa8674afc
haison0211 <haison.le286@gmail.com>
2021-10-11 07:10:37 +0000

    12312

--- jenkins/deploy/publish.sh
+++ jenkins/deploy/publish.sh
@@ -3,5 +3,5 @@ export IMAGE=$(sed -n '1p' /tmp/.auth)
 export PASS=$(sed -n '3p' /tmp/.auth)
 
 docker login -u haison123 -p $PASS
-cd /home/ubuntu/maven && docker-compose -f docker-compose-ang.yml up -d
+cd /home/ubuntu/maven && docker-compose up -d
 
