0bd4094e8778577c63ed5deb09098f1609771efe
haison0211 <haison.le286@gmail.com>
2022-03-05 00:20:06 +0700

    update

--- jenkins/push/push.sh
+++ jenkins/push/push.sh
@@ -4,8 +4,6 @@ echo  "***********************"
 echo  "******Pushing img *****"
 
 echo "****Logging docker hub ****"
-echo "pass is" $PASS
-docker login -u haison123 -p $PASS
 echo "***Tagging image***"
 docker tag $IMAGE_NAME:$IMAGE_VERSION haison123/$IMAGE_NAME:$IMAGE_VERSION
 echo "***Push image****"
