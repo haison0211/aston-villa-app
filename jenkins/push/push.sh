44c5755aa58571898aeae846da4effc7884f05b7
haison0211 <haison.le286@gmail.com>
2022-03-05 00:26:48 +0700

    update

--- jenkins/push/push.sh
+++ jenkins/push/push.sh
@@ -7,7 +7,6 @@ echo "****Logging docker hub ****"
 echo "***Tagging image***"
 docker tag $IMAGE_NAME:$IMAGE_VERSION haison123/$IMAGE_NAME:$IMAGE_VERSION
 echo "***Push image****"
-rm -rf ~/.dockercfg ~/.docker/config.json
-docker login -u AWS -p $(aws ecr get-login-password --region ap-east-1) 179623033511.dkr.ecr.ap-east-1.amazonaws.com
+aws ecr get-login-password --region ap-east-1 | docker login --username AWS --password-stdin 179623033511.dkr.ecr.ap-east-1.amazonaws.com
 docker tag $IMAGE_NAME:$IMAGE_VERSION 179623033511.dkr.ecr.ap-east-1.amazonaws.com/$IMAGE_NAME:$IMAGE_VERSION
 docker push 179623033511.dkr.ecr.ap-east-1.amazonaws.com/$IMAGE_NAME:$IMAGE_VERSION
